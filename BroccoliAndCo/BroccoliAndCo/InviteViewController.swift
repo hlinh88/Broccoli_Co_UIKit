dd187954032ff674dc9d76a71d9570bacbbb8e02
Hoang Linh <hlinh.jobs@gmail.com>
2023-02-20 13:33:29 +0700

    build invite form view

---
+++ BroccoliAndCo/BroccoliAndCo/InviteViewController.swift
@@ -0,0 +1,184 @@
+//
+//  InviteViewController.swift
+//  BroccoliAndCo
+//
+//  Created by Hoang Linh Nguyen on 19/2/2023.
+//
+
+
+import UIKit
+import RSFormView
+
+
+class InviteViewController: UIViewController, FormViewDelegate{
+    var formView = FormView()
+    var submitButton = UIButton(type: .custom)
+    var titleLabel = UILabel()
+    var dismissButton = UIButton()
+    
+    var formHelper = InviteViewModel()
+    
+    var name: String = ""
+    var email: String = ""
+    var errMess: String = ""
+    var isSuccess: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        //configure all the view elements and contraints
+        configureViews()
+        //bar button to dismiss the view
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(dismissSelf))
+    }
+    
+    @objc private func dismissSelf(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //configure all UI elements
+    func configureViews(){
+        configureFormView()
+        configureLabels()
+        configureSubmitButton()
+        configureConstraints()
+    }
+    
+    func configureDismissButton(){
+        dismissButton.setImage(UIImage(named: "imgName"), for: .normal)
+    }
+    
+    func configureSubmitButton(){
+        updateSubmitButton(enabled: false)
+        submitButton.layer.cornerRadius = 8
+        submitButton.setTitle("Send", for: .normal)
+        submitButton.setTitleColor(UIColor.white, for: .normal)
+        submitButton.titleLabel?.font =  UIFont(name: "MusticaPro-SemiBold", size: 18)
+        submitButton.layer.borderWidth = 2
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.addTarget(self, action: #selector(submitButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(submitButton)
+    }
+    
+    func configureLabels(){
+        titleLabel.numberOfLines = 0
+        titleLabel.lineBreakMode = .byWordWrapping
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = UIColor.black
+        titleLabel.textAlignment = .center
+        titleLabel.text = "Request an invite"
+        titleLabel.font = UIFont(name: "MusticaPro-SemiBold", size: 25)
+        titleLabel.addBorder(toSide: .bottom, withColor: UIColor.brightGray.cgColor, andThickness: 2.0)
+        self.view.addSubview(titleLabel)
+    }
+    
+    
+    func configureFormView() {
+        formView.delegate = self
+        formView.viewModel = formHelper //configure the form using the model
+        formView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(formView)
+    }
+    
+    func configureConstraints(){
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            titleLabel.bottomAnchor.constraint(equalTo: formView.topAnchor, constant: -100),
+            
+            formView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            formView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            formView.bottomAnchor.constraint(equalTo: submitButton.topAnchor, constant: -10),
+            
+            submitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            submitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            submitButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25),
+            submitButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    func updateSubmitButton(enabled: Bool) {
+        let backgroundColor = enabled ? UIColor.white : UIColor.brightGray.withAlphaComponent(0.4)
+        let textColor = enabled ? UIColor.black : UIColor.white
+        let borderColor = enabled ? UIColor.black.cgColor : UIColor.clear.cgColor
+        submitButton.backgroundColor = backgroundColor
+        submitButton.setTitleColor(textColor, for: .normal)
+        submitButton.layer.borderColor = borderColor
+        submitButton.isUserInteractionEnabled = enabled
+    }
+    
+    func didUpdateFields(in formView: FormView, allFieldsValid: Bool){
+        updateSubmitButton(enabled: allFieldsValid)
+    }
+    
+    @objc
+    func submitButtonTapped(sender: UIButton!) {
+        sender.backgroundColor = UIColor.black
+        sender.setTitleColor(UIColor.white, for: .normal)
+        sender.isSelected = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            sender.backgroundColor = UIColor.white
+            sender.setTitleColor(UIColor.black, for: .normal)
+            sender.isSelected = false
+        }
+        print("Hi")
+    }
+    
+}
+
+
+class InviteViewModel: FormViewModel {
+    
+    var items: [FormItem] = []
+    
+    init() {
+        items = [NameItem(), EmailItem(),
+                 confirmEmailItem()]
+    }
+    
+    enum FieldName: String {
+        case name = "FULL NAME"
+        case email = "EMAIL"
+        case confirmEmail = "CONFIRM EMAIL"
+    }
+
+    //Validate the name
+    func NameItem() -> FormItem {
+        let nameField = FormField(name: FieldName.name.rawValue,
+                                  initialValue: "",
+                                  placeholder: FieldName.name.rawValue,
+                                  fieldType: .regular,
+                                  isValid: false,
+                                  errorMessage: "Full name must be at least 3 characters long")
+        nameField.minimumTextLength = 3
+        return TextFieldCellItem(with: nameField)
+    }
+    
+    //validate the email
+    func EmailItem() -> FormItem {
+        let emailField = FormField(name: FieldName.email.rawValue,
+                                   initialValue: "",
+                                   placeholder: FieldName.email.rawValue,
+                                   fieldType: .email,
+                                   isValid: false,
+                                   errorMessage: "Please enter a valid email")
+        return TextFieldCellItem(with: emailField)
+    }
+    
+    //check if the emails match
+    func confirmEmailItem() -> FormItem {
+        let confirmEmailField = FormField(name: FieldName.confirmEmail.rawValue,
+                                          initialValue: "",
+                                          placeholder: FieldName.confirmEmail.rawValue,
+                                          fieldType: .email,
+                                          isValid: false,
+                                          errorMessage: "Email doesn't match")
+        confirmEmailField.validationMatch = FieldName.email.rawValue
+        
+        return TextFieldCellItem(with: confirmEmailField)
+    }
+}
+
--- BroccoliAndCo/BroccoliAndCo/ViewController.swift
+++ BroccoliAndCo/BroccoliAndCo/ViewController.swift
@@ -10,19 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     var isHighLighted : Bool = false
-    
-    //image view loading the image on the screen
-    let someImageView: UIImageView = {
-        let theImageView = UIImageView()
-        theImageView.image = UIImage(named: "Logo.png") //image path
-        theImageView.translatesAutoresizingMaskIntoConstraints = false // the image is added to your view
-        return theImageView
-    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        view.addSubview(someImageView)
-        //        someImageViewConstraints()
         buttonConstraints()
         subNameConstraints()
         nameConstraints()
@@ -40,31 +31,24 @@ class ViewController: UIViewController {
     //        }
     //    }
     
-    //    on button press, the invite details view is shown
+
     @objc func buttonAction(sender: UIButton!) {
-        //        let rootVC = InviteViewController()
-        //        let navVC = UINavigationController(rootViewController: rootVC)
-        //        navVC.modalPresentationStyle = .fullScreen
-        //        present(navVC, animated: true)
-        if sender.isSelected == true {
+        let navView = UINavigationController(rootViewController: InviteViewController())
+        navView.modalPresentationStyle = .formSheet
+        present(navView, animated: true)
+        
+        sender.backgroundColor = UIColor.black
+        sender.setTitleColor(UIColor.white, for: .normal)
+        sender.isSelected = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             sender.backgroundColor = UIColor.white
             sender.setTitleColor(UIColor.black, for: .normal)
             sender.isSelected = false
-            
-        } else {
-            sender.backgroundColor = UIColor.black
-            sender.setTitleColor(UIColor.white, for: .normal)
-            sender.isSelected = true
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                sender.backgroundColor = UIColor.white
-                sender.setTitleColor(UIColor.black, for: .normal)
-                sender.isSelected = false
-            }
         }
+      
     }
     
-    
-    //Constraints for the button
+
     func buttonConstraints() {
         let button = UIButton(frame: CGRect(x: 200, y: 200, width: 100, height: 10))
         button.backgroundColor =  UIColor.white //button color
@@ -72,26 +56,25 @@ class ViewController: UIViewController {
         button.setTitleColor(UIColor.black, for: .normal)
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 8
         button.titleLabel?.font =  UIFont(name: "MusticaPro-SemiBold", size: 17)
         button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 18, bottom: 10, right: 18)
         button.addTarget(self, action: #selector(buttonAction), for: .touchDown)
-        
         button.clipsToBounds = true
         button.tintColor = UIColor.white
-        self.view.addSubview(button)// add the button to the view, with constraints
+        self.view.addSubview(button)
         
-        button.translatesAutoresizingMaskIntoConstraints = false //dont use autolayout constraints
+        button.translatesAutoresizingMaskIntoConstraints = false //set autolayout constraints to false
         
-        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true //center the button on the x axis
-        button.bottomAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 140).isActive = true //set a bottom anchor to the button
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 140).isActive = true
         
     }
     
-    //constraints for the sub label
+
     func subNameConstraints(){
         let text = UILabel()
         text.frame = CGRect(x: 0, y: 0, width: text.frame.width, height: text.frame.height)
-        text.center = CGPoint(x: 160, y: 285)
         text.textAlignment = .center
         text.text = "Be the first to know when we launch."
         text.font = UIFont.systemFont(ofSize: 15)
@@ -107,11 +90,10 @@ class ViewController: UIViewController {
         text.bottomAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 70).isActive = true
     }
     
-    //constraints for the name label
+
     func nameConstraints() {
         let label = UILabel()
         label.frame = CGRect(x: label.frame.origin.x, y: label.frame.origin.y, width: label.frame.width, height: label.frame.height)
-        label.center = CGPoint(x: 160, y: 285)
         label.textAlignment = .center
         label.text = "A better way to enjoy every day."
         label.font = UIFont(name: "MusticaPro-SemiBold", size: 25)
@@ -131,9 +113,8 @@ class ViewController: UIViewController {
     func buttomTitleConstraints() {
         let label = UILabel()
         label.frame = CGRect(x: label.frame.origin.x, y: label.frame.origin.y, width: label.frame.width, height: label.frame.height)
-        label.center = CGPoint(x: 160, y: 285)
         label.textAlignment = .center
-        label.text = "Made with ❤ in Melbourne. \n© 2023 Broccoli & Co. All rights reserved."
+        label.text = "© 2023 Broccoli & Co. All rights reserved."
         label.font = UIFont.systemFont(ofSize: 10)
         label.numberOfLines = 0;
         label.sizeToFit()
@@ -146,12 +127,11 @@ class ViewController: UIViewController {
         label.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20).isActive = true
         
     }
-    
-    
-    // title label constraints
+
     func titleConstraints() {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 0, width: label.frame.width, height: label.frame.height)
+
         label.textAlignment = .center
         label.text = "Brocolli & Co."
         label.font = UIFont(name: "MusticaPro-SemiBold", size: 25)
@@ -160,18 +140,9 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         
         label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 70).isActive = true //set at the top of the view
+        label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 70).isActive = true
     }
-    
-    //image constraints
-    func someImageViewConstraints() {
-        someImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        someImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        someImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        someImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10).isActive = true //position the image in the center of the view
-        someImageView.contentMode = .scaleAspectFit //make the image scale to fit
-    }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         // Hide the Navigation Bar
